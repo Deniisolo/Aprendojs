dd1013772711788e808cb23199f50054355cf614
Denisolo <deniisolo@outlook.es>
2023-05-12 16:56:14 -0500

    modifiqué el alert

--- Login 2/index.js
+++ Login 2/index.js
@@ -2,6 +2,6 @@ function boton (){
     let usuario= document.getElementById("user").value;
     let contraseña= document.getElementById("password").value;
     if ( usuario=="deniisolo@gmail.com" && contraseña=="discordesgenial"){
-        alert("aceptada")
+        alert("aceptadoooo")
     }else {alert ("rechazada")}
 }
