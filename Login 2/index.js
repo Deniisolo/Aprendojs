14c46e9685de8e83db455cb1f9d27baa814fd15d
Denisolo <deniisolo@outlook.es>
2023-05-12 16:42:44 -0500

    agregué js

---
+++ Login 2/index.js
@@ -0,0 +1,7 @@
+function boton (){
+    let usuario= document.getElementById("user").value;
+    let contraseña= document.getElementById("password").value;
+    if ( usuario=="deniisolo@gmail.com" && contraseña=="discordesgenial"){
+        alert("aceptada")
+    }else {alert ("rechazada")}
+}
