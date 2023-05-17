092b9b4b6f9f50fce310235ec8f9709f95d35590
Denisolo <deniisolo@outlook.es>
2023-05-17 12:00:51 -0500

    realicé ejercicio de booleans

--- booleans/index.js
+++ booleans/index.js
@@ -16,3 +16,32 @@ if (num % 2===0){
 }
 esPar(5)
 
+function esPositivo(numero){
+if (numero > 0) {
+  return true
+}else{
+   return false
+}
+}
+esPositivo(1)
+
+function esVocal(letra){
+  if (letra === "a") {
+   return true
+  }
+  if (letra === "e"){
+   return true
+  }
+  if (letra === "i"){
+   return true
+  }
+  if (letra === "o"){
+   return true
+  }
+  if (letra === "u"){
+   return true
+  } 
+  else{
+   return false
+  }
+}
