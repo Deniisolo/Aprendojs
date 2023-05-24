5d1474625be8325f6ab587eba6ba7a8cfcf0795d
Denisolo <deniisolo@outlook.es>
2023-05-24 16:17:15 -0500

    ejercicios de condicionales

---
+++ Condicionales/index.js
@@ -0,0 +1,17 @@
+//Ejercicio 1: Verificar si un número es positivo, negativo o cero.
+let num= 0;
+if (num > 0) {
+    console.log("positivo")}
+    else if ( num < 0){
+        console.log("negativo")
+    }
+    else {
+        console.log( "es cero")
+    }
+
+
+
+
+
+
+
