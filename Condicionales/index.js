c3700ecc9b0ac25ad5e605ffa308961652d87f7e
Denisolo <deniisolo@outlook.es>
2023-05-25 11:53:41 -0500

    otro ejrcicio de condicionales

--- Condicionales/index.js
+++ Condicionales/index.js
@@ -1,11 +1,33 @@
 //Ejercicio 1: Verificar si un número es positivo, negativo o cero.
+// let numero = 7;
+
+// if (numero > 0) {
+//   console.log("El número es positivo");
+// } else if (numero < 0) {
+//   console.log("El número es negativo");
+// } else {
+//   console.log("El número es cero");
+// }
 
 //Ejercicio 2: Determinar si un número es par o impar.
-function esPar(num1) {
-  if (num1 % 2 === 0) {
-    console.log("es par");
+// function esPar(num1) {
+//   if (num1 % 2 === 0) {
+//     console.log("es par");
+//   } else {
+//     console.log("no es par");
+//   }
+// }
+//esPar(7);
+
+//Ejercicio 1:Escribe una función llamada esMayorDeEdad que reciba
+//como parámetro la edad de una persona y devuelva "Es mayor de edad"
+//si la edad es igual o mayor a 18, y "No es mayor de edad" en caso contrario.
+
+function esMayorDeEdad(edad) {
+  if (edad > 18) {
+    return "es mayor de edad";
   } else {
-    console.log("no es par");
+    return "no puede entrar a la disco";
   }
 }
-esPar(7);
+esMayorDeEdad();
