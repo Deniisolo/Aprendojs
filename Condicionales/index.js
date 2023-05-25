6bc38e9b335c3e7059e6a8f8cf57238f1c8b34dd
Denisolo <deniisolo@outlook.es>
2023-05-25 11:11:38 -0500

    agregué otro ejercicio

--- Condicionales/index.js
+++ Condicionales/index.js
@@ -1,17 +1,11 @@
 //Ejercicio 1: Verificar si un número es positivo, negativo o cero.
-let num= 0;
-if (num > 0) {
-    console.log("positivo")}
-    else if ( num < 0){
-        console.log("negativo")
-    }
-    else {
-        console.log( "es cero")
-    }
 
-
-
-
-
-
-
+//Ejercicio 2: Determinar si un número es par o impar.
+function esPar(num1) {
+  if (num1 % 2 === 0) {
+    console.log("es par");
+  } else {
+    console.log("no es par");
+  }
+}
+esPar(7);
