797850eb6d424cdc058a73640be24454055a3489
Denisolo <deniisolo@outlook.es>
2023-05-30 09:35:18 -0500

    ejercicio de switch

---
+++ switch/index.js
@@ -0,0 +1,46 @@
+// let dia = 8;
+
+// switch (dia) {
+//   case 1:
+//     console.log("Lunes");
+//     break;
+//   case 2:
+//     console.log("Martes");
+//     break;
+//   case 3:
+//     console.log("Miércoles");
+//     break;
+//   case 4:
+//     console.log("Jueves");
+//     break;
+//   case 5:
+//     console.log("Viernes");
+//     break;
+//   case 6:
+//     console.log("Sábado");
+//     break;
+//   case 7:
+//     console.log("Domingo");
+//     break;
+//   default:
+//     console.log("Día inválido");
+// }
+
+let mes = 2;
+
+switch (mes) {
+  case 1:
+    console.log("Enero");
+    break;
+  case 2:
+    console.log("Febrero");
+    break;
+  case 3:
+    console.log("Marzo");
+    break;
+  case 4:
+    console.log("Abril");
+    break;
+  default:
+    console.log("NO ESTÁ");
+}
