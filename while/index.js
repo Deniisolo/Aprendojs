01711fc06437388ba337380461f99d6c6018cedf
Denisolo <deniisolo@outlook.es>
2023-05-31 16:47:49 -0500

    ejercicio de while

--- while/index.js
+++ while/index.js
@@ -6,9 +6,21 @@
 //   contador=contador+1;
 // }
 
-let contador = 1;
+// let contador = 1;
 
-while (contador <= 5) {
-  console.log("Contador: " + contador);
-  contador++;
-}
+// while (contador <= 5) {
+//   console.log("Contador: " + contador);
+//   contador++;
+// }
+
+// let contador = 1;
+// while( contador <= 10) {
+//     console.log("Contador:" + contador);
+//     contador= contador + 1;
+// }
+
+// let contador = 1;
+// while ( contador <= 20 ) {
+//     console.log("contador igual a:" + contador)
+//     contador= contador +1;
+// }
