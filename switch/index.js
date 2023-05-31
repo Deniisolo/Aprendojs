0bba95ecc2a56561fa2a9a48d2c5f68d754c5034
Denisolo <deniisolo@outlook.es>
2023-05-31 12:10:26 -0500

    ejercicio de Switch

--- switch/index.js
+++ switch/index.js
@@ -77,32 +77,32 @@
 // y muestre si es un día laborable o un día de descanso utilizando
 // la estructura condicional switch.
 
-// let dialaborable = 0;
+let dialaborable = 0;
 
-// switch (dialaborable) {
-//   case 1:
-//     console.log("Lunes Día Laborable");
-//     break;
-//   case 2:
-//     console.log("Martes Día Laborable");
-//     break;
-//   case 3:
-//     console.log("Miercoles Día Laborable");
-//     break;
-//   case 4:
-//     console.log("Jueves Día Laborable");
-//     break;
-//   case 5:
-//     console.log("Viernes Día Laborable");
-//     break;
-//   case 6:
-//     console.log("Sábado Día Laborable");
-//     break;
-//   case 7:
-//     console.log("Domingo Día de DESCANSO");
-//     break;
-//   default:
-//     console.log("Día no valido");
-//     break;
-// }
+switch (dialaborable) {
+  case 1:
+    console.log("Lunes Día Laborable");
+    break;
+  case 2:
+    console.log("Martes Día Laborable");
+    break;
+  case 3:
+    console.log("Miercoles Día Laborable");
+    break;
+  case 4:
+    console.log("Jueves Día Laborable");
+    break;
+  case 5:
+    console.log("Viernes Día Laborable");
+    break;
+  case 6:
+    console.log("Sábado Día Laborable");
+    break;
+  case 7:
+    console.log("Domingo Día de DESCANSO");
+    break;
+  default:
+    console.log("Día no valido");
+    break;
+}
 //Calificación de CHATGPT: 9
