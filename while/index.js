b20eef1f5e580ad11da059f2e049898da6e2ca20
Denisolo <deniisolo@outlook.es>
2023-05-31 16:04:08 -0500

    ejercicios de while

---
+++ while/index.js
@@ -0,0 +1,14 @@
+// let contador = -1;
+// const limite = 10;
+
+// while (contador <= limite) {
+//   console.log(contador);
+//   contador=contador+1;
+// }
+
+let contador = 1;
+
+while (contador <= 5) {
+  console.log("Contador: " + contador);
+  contador++;
+}
