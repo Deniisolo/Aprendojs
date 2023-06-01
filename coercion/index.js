0a3b495d79341e7eb6806aa0f2e705437493ab22
Denisolo <deniisolo@outlook.es>
2023-06-01 11:50:59 -0500

    ejecicio de coerción

--- coercion/index.js
+++ coercion/index.js
@@ -1,4 +1,9 @@
+// let numero= 5;
+// let texto = "8";
+// let resultado = numero + texto;
+// console.log(resultado);
+
 let numero= 5;
-let texto = "8";
-let resultado = numero + texto;
-console.log(resultado);
+let texto = "5";
+let resultado= numero + Number(texto);
+console.log(resultado)
