d971596e11c38e19bf1eaaf2f8a2cfec34acbd83
Denisolo <deniisolo@outlook.es>
2023-06-05 17:26:56 -0500

    login 3

---
+++ Login3/index.js
@@ -0,0 +1,11 @@
+function Boton() {
+    let user = document.getElementById("user").value;
+    let password = document.getElementById("password").value;
+   if (user == "deniisolo@gmail.com" && password == "discordesgenial"){
+    alert("BIENVENIDAD")
+   }else{
+    alert("hola")
+   }
+}
+Boton()
+
