fca156ff9e1337394a3e946de8081ca15beb0677
Denisolo <deniisolo@outlook.es>
2023-06-09 09:30:21 -0500

    ejemplo de Array

---
+++ Array/index.js
@@ -0,0 +1,3 @@
+//en la consola nos nuestra una lista de las frutas 
+frutas = ["manzana", "plátano", "naranja", "uva", "sandía", "piña", "mango", "fresa", "kiwi", "limón", "pera", "cereza", "melón"]
+console.log(frutas)
