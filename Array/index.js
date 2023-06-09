b2e6637c2e04e5e452a2a63ff92cfc01cdd6eb4f
Denisolo <deniisolo@outlook.es>
2023-06-09 09:43:03 -0500

    ejemplos de arrays

--- Array/index.js
+++ Array/index.js
@@ -1,3 +1,16 @@
 //en la consola nos nuestra una lista de las frutas 
-frutas = ["manzana", "plátano", "naranja", "uva", "sandía", "piña", "mango", "fresa", "kiwi", "limón", "pera", "cereza", "melón"]
+let frutas = ["manzana", "plátano", "naranja", "uva", "sandía", "piña", "mango", "fresa", "kiwi", "limón", "pera", "cereza", "melón"]
+console.log(frutas)
+
+// para saber cuantos elemento tiene Array 
+
+console.log(frutas.length); 
+
+// para aceder a un Array en especifico se comienza a contar desde (0)
+
+console.log(frutas[3]);
+
+// para agregar mas datos en el array pero al final de la lista 
+
+let masfrutas= frutas.push("Tomate")
 console.log(frutas)
