31badb35e5657f2d51ee9467c0f9530904a2c631
Denisolo <deniisolo@outlook.es>
2023-06-11 21:18:10 -0500

    elinimé documentos de Html y Js

--- Array/index.js
+++ Array/index.js
@@ -2,30 +2,35 @@
 let frutas = ["manzana", "plátano", "naranja", "uva", "sandía", "piña", "mango", "fresa", "kiwi", "limón", "pera", "cereza", "melón"]
 console.log(frutas)
 
-// para saber cuantos elemento tiene Array 
+// // para saber cuantos elemento tiene Array 
 
 console.log(frutas.length); 
 
-// para aceder a un Array en especifico se comienza a contar desde (0)
+// // para aceder a un Array en especifico se comienza a contar desde (0)
 
 console.log(frutas[3]);
 
-// para agregar mas datos en el array pero al final de la lista 
+// // para agregar mas datos en el array pero al final de la lista 
 
 let masfrutas= frutas.push("Tomate")
 console.log(frutas)
 
-//Para eliminar el último elemento de la lista 
+// //Para eliminar el último elemento de la lista 
 
 let ultimo = frutas.pop("Tomate")
 console.log(frutas)
 
-// para poner un nuevo elemento en la lista pero que quede de primeras
+// // para poner un nuevo elemento en la lista pero que quede de primeras
 
 let nuevoelemento = frutas.unshift("guanabana")
 console.log(frutas)
 
-//para eliminar un elemento que este de primeras
+// //para eliminar un elemento que este de primeras
 
 let eleminar1 = frutas.shift("guanabana")
 console.log(frutas)
+
+// // para saber la posición de un elemento :3 cuando conoces el nombre del elemento 
+
+let posicion = frutas.indexOf("limón")
+console.log(posicion)
