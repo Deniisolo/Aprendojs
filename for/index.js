09e5ce3e4535bdcf4c34642c7712105b576251ac
Denisolo <deniisolo@outlook.es>
2023-06-13 16:36:58 -0500

    ejemplo de FOR

---
+++ for/index.js
@@ -0,0 +1,9 @@
+// sintaxis de FOR es ICI: inicio-condición-incremento
+// for (inicio; condición; incremento) {
+//     bloque de código a ejecutar
+//   }
+// Ejemplo:
+
+for (let i = 0; i < 5; i++) {
+  console.log(i);
+}
