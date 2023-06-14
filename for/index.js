1930b33457574e91ccc9b05ae33b82d7e2cfb0a4
Denisolo <deniisolo@outlook.es>
2023-06-14 14:30:59 -0500

    ejercicio de FOR

--- for/index.js
+++ for/index.js
@@ -7,3 +7,8 @@
 for (let i = 0; i < 5; i++) {
   console.log(i);
 }
+var nombres = ["Andres", "Diego", "Platzi", "Ramiro", "Silvia"]
+
+for(var i = 0; i < nombres.length; i++){
+    console.log( nombres[i] )
+}
