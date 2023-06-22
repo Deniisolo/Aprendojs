43cd45d93099d228ca0c888ca9ba06d1f88ad52d
Denisolo <deniisolo@outlook.es>
2023-06-22 11:32:05 -0500

    ejercicio de recorridos de Arrays

---
+++ recorridosdeArrays/index.js
@@ -0,0 +1,14 @@
+let articulos = [
+    { nombre: "Bici", costo: 3000},
+    { nombre: "Tv", costo: 2500 },
+    { nombre: "Libro", costo: 320 },
+    { nombre: "Celular", costo: 10000 },
+    { nombre: "Laptop", costo: 20000 },
+    { nombre: "Teclado", costo: 500},
+    { nombre: "Audifonos", costo: 1700},  
+];
+
+let articulosFiltrados = articulos.filter(function(articulo){
+    return articulo.costo <= 500
+});
+
